07aabf49fe559ddf8569d15a56ed9d7bde4a71db
VincentZhang <vincent.zhang@ualberta.ca>
2020-08-08 17:56:10 -0600

    update the example to include a fixed random linear policy

--- example.py
+++ example.py
@@ -6,19 +6,26 @@ mdp = MDP_LQR_Disc(fn='env/mdp_disc_laplacian.json')
 
 print("The starting state is {}".format(mdp.state))
 
-# random policy
+# Get a random linear policy K and run it for 10 steps
+K = np.random.random((mdp.dim_x, mdp.dim_u))
 for i in range(10):
     # take random actions and print the state
-    action = np.random.random(mdp.dim_u)
-    next_state, reward, done = mdp.step(action)
-    print("Iter {}: Next state: {}, cost: {}, done: {}".format(mdp.iter, next_state, reward, done))
-
-# optimal policy
-mdp.reset()
-print("The state is reset to {}".format(mdp.state))
-K = mdp.get_optimal_policy()
-for i in range(10):
-    # take optimal action and print the state
     action = K.dot(mdp.state)
     next_state, reward, done = mdp.step(action)
     print("Iter {}: Next state: {}, cost: {}, done: {}".format(mdp.iter, next_state, reward, done))
+# Print the Value Matrix P for the current policy K where V(x) = x'Px
+mdp.compute_cost_matrix(K)
+print("The value matrix P: \n", mdp.P)
+
+# Get the optimal policy and run it for 10 steps
+K_optimal = mdp.get_optimal_policy()
+mdp.reset()
+print("The state is reset to {}".format(mdp.state))
+for i in range(10):
+    # take optimal action and print the state
+    action = K_optimal.dot(mdp.state)
+    next_state, reward, done = mdp.step(action)
+    print("Iter {}: Next state: {}, cost: {}, done: {}".format(mdp.iter, next_state, reward, done))
+
+# Print the Optimal Value Matrix P_optimal where V(x) = x'P_optimal x
+print("The optimal value matrix P_optimal: \n", mdp.P_optimal)
