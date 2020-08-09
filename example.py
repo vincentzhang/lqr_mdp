b725d07684d467be1a55fc14f9b5a10a2d848338
VincentZhang <vincent.zhang@ualberta.ca>
2020-08-08 18:23:42 -0600

    add termination check in the example

--- example.py
+++ example.py
@@ -8,11 +8,16 @@ print("The starting state is {}".format(mdp.state))
 
 # Get a random linear policy K and run it for 10 steps
 K = np.random.random((mdp.dim_x, mdp.dim_u))
-for i in range(10):
+for _ in range(10):
     # take random actions and print the state
     action = K.dot(mdp.state)
     next_state, reward, done = mdp.step(action)
     print("Iter {}: Next state: {}, cost: {}, done: {}".format(mdp.iter, next_state, reward, done))
+    if done:
+        # In this case, it's either the cost is bigger than the maximum, or that the state is NaN
+        print("Time to terminate")
+        break
+        
 # Print the Value Matrix P for the current policy K where V(x) = x'Px
 mdp.compute_cost_matrix(K)
 print("The value matrix P: \n", mdp.P)
@@ -21,7 +26,7 @@ print("The value matrix P: \n", mdp.P)
 K_optimal = mdp.get_optimal_policy()
 mdp.reset()
 print("The state is reset to {}".format(mdp.state))
-for i in range(10):
+for _ in range(10):
     # take optimal action and print the state
     action = K_optimal.dot(mdp.state)
     next_state, reward, done = mdp.step(action)
